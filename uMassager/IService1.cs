204467ac87ab4ce0c6d59f3484fb2674cf22a70c
óchiAnthropos <58050924+ochiantropos@users.noreply.github.com>
2023-02-23 16:37:31 +0200

    Refresh version + write the server servise base

---
+++ uMassager/IService1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace uMassager
+{
+    [ServiceContract(CallbackContract =typeof(IServerChatCallback))]
+    public interface IService1
+    {
+        [OperationContract]
+        int Connect();
+        
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMassage(string _massage);
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract]
+        void MassageCallback(string _massage);
+
+    }
+
+}
---
+++ uMassager/Service1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace uMassager
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)] // one servise for all user-connection
+    public class Service1 : IService1
+    {
+        public int Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMassage(string _massage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
